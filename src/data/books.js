db83a9d468badf4d64d5078cab47b2145bb01764
Vladislav Martsul <vladislav.martsul@intexsoft.by>
2021-04-08 11:25:05 +0300

    work w/ authors

--- src/data/books.js
+++ src/data/books.js
@@ -3,12 +3,16 @@ import Author from "../model/Author";
 const faker = require('faker');
 
 const books = [];
+const maxAuthAmount = 3;
+const maxBooksAmount = 15;
 
-for (let i = 0; i < 15; i++) {
+for (let i = 0; i < maxBooksAmount; i++) {
 
     const authorsArray = [];
-    const author:Author = new Author(faker.datatype.number(), faker.name.findName());
-    authorsArray.push(author)
+    for (let j = 0; j <= (Math.floor(Math.random() * maxAuthAmount)); j++){
+        const author:Author = new Author(faker.datatype.number(), faker.name.findName());
+        authorsArray.push(author)
+    }
 
     const book:Book = new Book(
         faker.datatype.number(),
@@ -19,7 +23,8 @@ for (let i = 0; i < 15; i++) {
         faker.datatype.boolean().toString(),
         faker.date.past().getFullYear(),
         faker.music.genre(),
-        faker.datatype.number())
+        faker.datatype.number()
+    )
     console.log(book)
 
     books.push(book);
