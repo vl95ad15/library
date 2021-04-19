a80d432cd002bd008eeff4beb85952e3b0ef09e2
Vladislav Martsul <vladislav.martsul@intexsoft.by>
2021-04-19 16:41:57 +0300

    commenting unnecessary code

--- src/components/layout/filter/FilterItem.jsx
+++ src/components/layout/filter/FilterItem.jsx
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
-
-function FilterItem(props) {
-    const [checked, setChecked] = useState(false);
-
-    return(
-        <label>
-            <input type='checkbox' checked={checked} onChange={() => setChecked(!checked)}/>
-            {props.text}
-        </label>
-    )
-}
-
-export default FilterItem
+// import React, { useState } from "react";
+//
+// function FilterItem(props) {
+//     const [checked, setChecked] = useState(false);
+//
+//     return(
+//         <label>
+//             <input type='checkbox' checked={checked} onChange={() => setChecked(!checked)}/>
+//             {props.text}
+//         </label>
+//     )
+// }
+//
+// export default FilterItem
